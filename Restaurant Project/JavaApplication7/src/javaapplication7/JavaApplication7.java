09eb419f5f8951cccaa1f6591e8bf6bf12d2d354
Tofy123 <latifah.0330@gmail.com>
2022-09-08 20:58:33 +0300

    my first commit

---
+++ Restaurant Project/JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author user
+ */
+import javax.swing.JFrame;
+public class JavaApplication7{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        PROJACT frm = new PROJACT();
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm.setSize(1038,537);
+        frm.setVisible(true);
+    }
+    
+}
